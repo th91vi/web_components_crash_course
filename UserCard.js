23fc0ab3de99a9f2ca9513e77c0e743c2a03914c
Thiago Vieira <th91v@outlook.com>
2020-03-17 18:57:35 -0300

    lógica para esconder ou exibir informações nos cartões

--- UserCard.js
+++ UserCard.js
@@ -41,12 +41,41 @@ class UserCard extends HTMLElement {
     constructor(){
         super();
 
+        this.showInfo = true;
+
         // this.innerHTML = `<style>h3 { color: coral }</style> <h3>${this.getAttribute('name')}</h3>`;
         
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(userCardTemplate.content.cloneNode(true));
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
         this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
+    }
+
+    toggleInfo(){
+        this.showInfo = !this.showInfo;
+        
+        const info = this.shadowRoot.querySelector('.info');
+        const toggleBtn = this.shadowRoot.querySelector('.util_toggle-info');
+        
+        if (this.showInfo) {
+            info.style.display = 'block';
+            toggleBtn.innerText = 'Hide Info';
+        } else {
+            info.style.display = 'none';
+            toggleBtn.innerText = 'Show Info';
+        }
+    }
+
+    connectedCallback() {
+        this.shadowRoot.querySelectorAll('.util_toggle-info').forEach(toggle => {
+            toggle.addEventListener('click', () => this.toggleInfo())
+        })
+    }
+
+    disconnectedCallback(){
+        this.shadowRoot.querySelectorAll('.util_toggle-info').forEach(toggle => {
+            toggle.removeEventListener();
+        })
     }
 }
 
