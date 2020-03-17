fa1e123ccaf1b4262ec5e17c475c52170fb95bda
Thiago Vieira <th91v@outlook.com>
2020-03-17 17:42:50 -0300

    customização de componente, com sobre posição global de estilo

--- UserCard.js
+++ UserCard.js
@@ -2,7 +2,7 @@ class UserCard extends HTMLElement {
     constructor(){
         super();
 
-        this.innerHTML = `John Doe`;
+        this.innerHTML = `<style>h3 { color: coral }</style> <h3>${this.getAttribute('name')}</h3>`;
     }
 }
 
