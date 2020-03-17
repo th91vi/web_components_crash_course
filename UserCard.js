b73fa6daad9760eaac08ab824ef487fb9f1a5f79
Thiago Vieira <th91v@outlook.com>
2020-03-17 18:08:54 -0300

    estilização de componentes, limitando seu escopo usando o shadowRoot

--- UserCard.js
+++ UserCard.js
@@ -1,8 +1,24 @@
+const userCardTemplate = document.createElement('template');
+userCardTemplate.innerHTML = `
+    <style>
+        h3 {
+            color: coral;
+        }
+    </style>
+    <div class="user-card">
+        <h3></h3>
+    </div>
+`;
+
 class UserCard extends HTMLElement {
     constructor(){
         super();
 
-        this.innerHTML = `<style>h3 { color: coral }</style> <h3>${this.getAttribute('name')}</h3>`;
+        // this.innerHTML = `<style>h3 { color: coral }</style> <h3>${this.getAttribute('name')}</h3>`;
+        
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(userCardTemplate.content.cloneNode(true));
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
     }
 }
 
